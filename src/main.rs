930558ee3c01b63daed3b0633aa1bcf3c7eb49bd
kannanATexcelforte <kannan@excelforte.com>
2024-01-05 11:34:16 +0530

    05.01.2024-cf
    completed

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,15 @@ fn add(num_one: i32, num_two: i32) -> i32 {
 }
 
 fn main() {
-    let foo = add(10, 5);
-    println!("{:?}", foo);
+    let total = add(10, 15);
+
+    if total > 50 {
+        println!("You qualify for free shipping!");
+    } else if total > 20 {
+        println!("If you add more items, you can qualify for free shipping.");
+    } else {
+        println!("No free shipping");
+    }
+
+    println!("{:?}", total);
 }
